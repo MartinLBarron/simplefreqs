7dea07059b122e0d2e58c9fb4eed1271d7b290d4
Martin Barron <martin@barron.cc>
2020-01-09 19:34:28 -0600

    catch-up commit

--- R/testing_freq.R
+++ R/testing_freq.R
@@ -2,11 +2,11 @@
 #Testing
 
 # testing -----------------------------------------------------------------
-# 
+#
 # library(forcats)
 # library(dplyr)
 # dfgss <-gss_cat
-# 
+#
 # # Simple, single ------------------------------------------------------------------
 # freq(iris,Species)
 # freq(dfgss, year)
@@ -16,37 +16,37 @@
 
 # # # Multiple variables ------------------------------------------------------
 # freq(dfgss, year, marital, partyid)
-# 
+#
 # # # whole dataset -----------------------------------------------------------
 # dfgss1 <- select(dfgss, year, marital, race, partyid)
 # freq(dfgss1)
-# 
+#
 # # # save results ------------------------------------------------------------
 # test2 <- freq(dfgss, year, saveResults = T)
 # test3 <- freq(dfgss, year, saveResults = F)
 
-# 
+#
 # # sort --------------------------------------------------------------------
 # freq(dfgss, year, sortResults = F)
 # freq(dfgss, year, sortResults = T)
-# 
+#
 # # print Results -----------------------------------------------------------
 # freq(dfgss, year, printResults = F)
 # freq(dfgss, year, printResults = T)
-# 
+#
 # # plot Results -----------------------------------------------------------
 # freq(dfgss, year, plotResults = F)
 # freq(dfgss, year, plotResults = T)
-# 
-# 
+#
+#
 # # Level warnings ----------------------------------------------------------
 # df <- freq(dfgss, denom, levelError = T)
 # df <- freq(dfgss, denom, levelError = F, sortResults = T, saveResults = T)
-# 
+#
 # # Missings ----------------------------------------------------------------
-# 
+#
 # dfgss$year[c(seq(1,1000, by=4))] <- NA
-# 
+#
 # x <-freq(dfgss, year)
 # x <-freq(dfgss, year, na.rm = T)
 
@@ -55,4 +55,4 @@
 # df <- dfgss %>%
 #   filter(year>2006) %>%
 #   freq(year)
-# 
+#
