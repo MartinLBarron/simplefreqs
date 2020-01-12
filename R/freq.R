c010cf5bdf9a3d0bedc112bc81c1d85679ba2aae
Martin Barron <martin@barron.cc>
2020-01-12 12:10:49 -0600

    Fixed description and prepped for .2 version

--- R/freq.R
+++ R/freq.R
@@ -6,17 +6,16 @@
 #' \code{freq} prints a frequency table for each variable passed to it.
 #' 
 #' @details
-#' This function prints a frequency table for variables passed to it.  In
+#' This function prints a frequency table for the variable passed to it.  In
 #' order to fit within the tidyverse, it takes as its first argument a dataframe
-#' and returns the frequency table as a dataframe. See ]code{freqMd} for version
-#' appropriate for easy markdown printing.
+#' and returns the frequency table as a dataframe. 
 #'
 #' @param df A dataframe (You can also pass this program a single variable and it will silently transform it to a dataframe)
-#' @param ... a variable or variables in associated dataframe. If you pass no variables, freq will be run on all variables in dataframe
+#' @param var a variable in associated dataframe
 #' @param plot if TRUE (default) prints bar chart of results.  If FALSE, no chart.
 #' @param sort If TRUE (default), sort output in descending order of n. If FALSE, sort output in ascending order of levels
 #' @param na.rm if FALSE (default) NAs are included in frequency list.  If TRUE, NA are removed (but reported seperately)
-#' @return  dataframe containing frequencies. Optionally prints frequency table and plots frequency
+#' @return  dataframe containing frequencies.
 #' @examples
 #' \dontrun{
 #' freq(iris, Species)
@@ -28,105 +27,74 @@
 #' @export
 #' 
 
-freq <- function(df, ..., plot=T, sort=T, na.rm=F){
+freq <- function(df, var=NA, plot=T, sort=T, na.rm=F){
+  
   
   #check if dataframe. If not, translate to dataframe
+  #This allows you to feed it either a vector or a dataframe/variable combo
   if (!is.data.frame(df)){
     dfname <- deparse(substitute(df))
+    print(dfname)
     if (grepl("$", dfname, fixed=T)){
       dfname <- strsplit(dfname, "$", fixed=T)[[1]][[2]]
       df <- data.frame(x=df)
       names(df)<-c(dfname)
-    } else{
+      var <- sym(dfname)
+    } else if (grepl("\"", dfname, fixed=T)){
       dfname <- strsplit(dfname, "\"", fixed=T)[[1]][[2]]
       df <- data.frame(x=df)
       names(df)<-c(dfname)
+      var <- sym(dfname)
+    } else{
+      df <- data.frame(x=df)
+      names(df)<-c(dfname)
+      var <- sym(dfname)
     }
+  } 
+  
+  enquo_x <- enquo(var)
+  
+  #Capture input variable for non-standard evaluation
+  
+  #remove NA if na.rm=T
+  if (na.rm==T){
+    naCount <- df %>%
+      filter(is.na(!! get_expr(enquo_x)))
+    df=filter(df, !is.na(!! get_expr(enquo_x)))
+    naCount1 <- nrow(naCount)
   }
-  #Save df as is for later return
-  df_orig=df
   
-  ## Deal with ...
-  #Capture all variables passed in
-  vars <- quos(...)
+  # The main meet of the function, calculate freqs here
+  df<-  df  %>%
+    mutate (temp=factor(!! enquo_x,exclude=NULL)) %>%
+    count(temp, sort=sort)  %>%
+    mutate(percentage = (n/sum(n))*100,
+           cumulative = cumsum(n),
+           cumulative_percent = (cumulative/sum(n))*100
+    )
   
-  #if no vars passed in, get all variables in dataframe
-  #this requires special "sym" command from rlang
-  if (length(vars)==0){
-    vars <- list()
-    nme <- names(df)
-    for (i in 1:length(nme)){
-      x <- nme[[i]]
-      gp <- quo(!! rlang::sym(x))
-      vars[[i]]<-unlist(gp)
-    }
+  #sort factor for chart
+  if (sort==T){
+    df[1] <- factor(df[[1]], levels = df[[1]][order(-df$n)])
   }
   
-  for (var in vars){
-    df <- df_orig
-    #Capture input variable for non-standard evaluation
-    enquo_x <- var
-    
-    #remove NA if na.rm=T
-    if (na.rm==T){
-      naCount <- df %>%
-        filter(is.na(!! get_expr(enquo_x)))
-      df=filter(df, !is.na(!! get_expr(enquo_x)))
-    }
-    
-    #Calculate frequencies
-    #note that count gives a warning because we're using factor to make a implicit
-    # NA level. To get arround with this we supress warnings temporarily
-    # but that requires a special margrittr operator %T>%
-    # df<-  df %T>%
-    #   {options(warn=-1)} %>%
-    #   count(factor(!! get_expr(enquo_x) ), sort=sortResults) %T>%
-    #   {options(warn=0)} %>%
-    #   mutate(percentage = (n/sum(n))*100,
-    #          cumulative = cumsum(n),
-    #          cumulative_percent = (cumulative/sum(n))*100
-    #   )
-
-    df<-  df  %>%
-      mutate (temp=factor(!! get_expr(enquo_x),exclude=NULL)) %>%
-      count(temp, sort=sort)  %>%
-      mutate(percentage = (n/sum(n))*100,
-             cumulative = cumsum(n),
-             cumulative_percent = (cumulative/sum(n))*100
-      )
-    
-    if (sort==T){
-      df[1] <- factor(df[[1]], levels = df[[1]][order(-df$n)])
-    }
-    
-    #Format name for printing
-    n <- names(df)
-    names(df) <- c(quo_name(enquo_x), "Freq", "Percent", "CumFreq", "CumPercent")
-    
-    #Set results class
-    #class(df) <- c("freqR_freq",class(df))
-    #print(class(df))
-    class(df) <- c("freqR_freq", "data.frame")
-    
-    attr(df, "title") <- quo_name(enquo_x)
-    #print(df)
-    
-    #Print results as requested
-    if (na.rm==T){
-      naCount1 <- nrow(naCount)
-      attr(df, "MissingRemoved") <- naCount1
-     }
-    
-    
-    #Plot results
-    if (plot==T){
-      gg <- ggplot(data=df, aes_string(quo_name(enquo_x), "Freq"))
-      gg <- gg + geom_bar(stat="identity")
-      gg <- gg + theme_minimal() + ggtitle (paste("Frequency:", quo_name(enquo_x))) + ylab("Count")
-      gg <- gg + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-      print (gg)
-    }
-    
+  #'     
+  #Format name in dataframe
+  names(df) <- c(quo_name(enquo_x), "Freq", "Percent", "CumFreq", "CumPercent")
+  
+  #Set results class
+  class(df) <- c("freqR_freq", "data.frame")
+  attr(df, "title") <- quo_name(enquo_x)
+  if (na.rm==T){
+    attr(df, "MissingRemoved") <- naCount1
+  }
+  #Plot results
+  if (plot==T){
+    gg <- ggplot(data=df, aes_string(quo_name(enquo_x), "Freq"))
+    gg <- gg + geom_bar(stat="identity")
+    gg <- gg + theme_minimal() + ggtitle (paste("Frequency:", quo_name(enquo_x))) + ylab("Count")
+    gg <- gg + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    print (gg)
   }
   
   return(df)
--- R/testing_freq.R
+++ R/testing_freq.R
@@ -3,59 +3,45 @@
 
 # testing -----------------------------------------------------------------
 
-library(forcats)
-library(dplyr)
-dfgss <-gss_cat
+# library(forcats)
+# library(dplyr)
+# dfgss <-gss_cat
 
 # Simple, single ------------------------------------------------------------------
-freq(iris,Species)
-freq(dfgss, year)
-freq(dfgss$year)
-freq(dfgss[,"year"], sort=F)
-x <-freq(iris,Species)
-print(x)
-x
-
+# freq(iris,Species)
+# freq(dfgss, year)
+# freq(dfgss$year)
+# freq(dfgss[["year"]], sort=F) # This doesn't work because it's still passing a dataframe
+# x <-freq(iris,Species)
+# print(x)
+# y=dfgss$year
+# freq(y)
 # # Missings ----------------------------------------------------------------
 
-dfgss$year[c(seq(1,1000, by=4))] <- NA
+# dfgss$year[c(seq(1,1000, by=4))] <- NA
 
-freq(dfgss, year, na.rm=T)
-x <- freq(dfgss, year, na.rm = T)
-x
+# freq(dfgss, year)
+# freq(dfgss, year, na.rm=F)
+# freq(dfgss, year, na.rm=T)
+# x <- freq(dfgss, year, na.rm = T)
+# x
 
-freq(dfgss, year, sort=F, na.rm = F)
-
-
-# Multiple variables ------------------------------------------------------
-freq(dfgss, year, marital, partyid)
+# Multiple variables (DEFUNCT) ------------------------------------------------------
+#freq(dfgss, year, marital, partyid)
 #No longer working
-# # # whole dataset -----------------------------------------------------------
-# dfgss1 <- select(dfgss, year, marital, race, partyid)
-# freq(dfgss1)
-#
-# # # save results ------------------------------------------------------------
-# test2 <- freq(dfgss, year, saveResults = T)
-# test3 <- freq(dfgss, year, saveResults = F)
 
-#
+# # # whole dataset  (DEFUNCT) -----------------------------------------------------------
+ # dfgss1 <- select(dfgss, year, marital, race, partyid)
+ # freq(dfgss1)
+
 # # sort --------------------------------------------------------------------
-# freq(dfgss, year, sortResults = F)
-# freq(dfgss, year, sortResults = T)
-#
-# # print Results -----------------------------------------------------------
-# freq(dfgss, year, printResults = F)
-# freq(dfgss, year, printResults = T)
-#
-# # plot Results -----------------------------------------------------------
-# freq(dfgss, year, plotResults = F)
-# freq(dfgss, year, plotResults = T)
-#
-#
-# # Level warnings ----------------------------------------------------------
-# df <- freq(dfgss, denom, levelError = T)
-# df <- freq(dfgss, denom, levelError = F, sortResults = T, saveResults = T)
-#
+# freq(dfgss, year, sort = F)
+# freq(dfgss, year, sort = T)
+
+
+# # # plot Results -----------------------------------------------------------
+# freq(dfgss, year, plot = F)
+# freq(dfgss, year, plot = T)
 
 # # As part of a dplyr chain ------------------------------------------------
 #As part of chain
