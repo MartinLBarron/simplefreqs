2d94da1672da13b44780f9d9df525a438a40f731
Martin Barron <MartinLBarron@users.noreply.github.com>
2023-08-16 12:13:47 -0500

    corrected error causing test to fail.

--- tests/testthat/test-freq-edge-cases.R
+++ tests/testthat/test-freq-edge-cases.R
@@ -2,6 +2,7 @@
 # core formats ------------------------------------------------------------
 
 test_that("works if padding set small and total row is large", {
+  library(dplyr)
   options(SimpleFreqs.inner_table_padding = 1)
   data("storms")
   expect_snapshot(freq(storms, day, plot = F))
