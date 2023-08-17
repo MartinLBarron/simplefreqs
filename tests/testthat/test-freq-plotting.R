33485473f9f26fe699d005b77597885df976c555
Martin Barron <MartinLBarron@users.noreply.github.com>
2023-08-17 09:04:56 -0500

    Basic test of plotting

--- tests/testthat/test-freq-plotting.R
+++ tests/testthat/test-freq-plotting.R
@@ -1,17 +1,39 @@
 
 # Plotting ------------------------------------------------------------
 
-test_that("basic plotting", {
+test_that("basic plotting1", {
   library(ggplot2)
-  save_png <- function() {
-    freq(iris, Species)
+  
+  save_png <- function(code, width = 400, height = 400) {
     path <- tempfile(fileext = ".png")
-    ggsave(path)
-    print(path)
+    png(path, width = width, height = height)
+    print(freq(iris, Species))
+    on.exit(dev.off())
+    
     path
   }
-
-  expect_snapshot_file(save_png(), "plot.png")
+    expect_snapshot_file(save_png(), "plot1.png")
+  
+    # You'd then also provide a helper that skips tests where you can't
+    # be sure of producing exactly the same output
+    expect_snapshot_plot <- function(name, code) {
+      # Other packages might affect results
+      skip_if_not_installed("ggplot2", "2.0.0")
+      # Or maybe the output is different on some operation systems
+      skip_on_os("windows")
+      # You'll need to carefully think about and experiment with these skips
+      
+      name <- paste0(name, ".png")
+      
+      # Announce the file before touching `code`. This way, if `code`
+      # unexpectedly fails or skips, testthat will not auto-delete the
+      # corresponding snapshot file.
+      announce_snapshot_file(name = name)
+      
+      path <- save_png(code)
+      expect_snapshot_file(path, name)
+    }
+    
 })
 
 
