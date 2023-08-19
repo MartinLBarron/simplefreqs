526497b1fec01632c4ced8939cb20fc08e79579b
Martin Barron <MartinLBarron@users.noreply.github.com>
2023-08-19 10:19:50 -0500

    Added pkgdown

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# SimpleFreqs 0.3.0
+
+* Initial CRAN submission.
--- tests/testthat/test-freq-plotting.R
+++ tests/testthat/test-freq-plotting.R
@@ -12,28 +12,10 @@ test_that("basic plotting1", {
     
     path
   }
+  
+    skip_on_cran()
     expect_snapshot_file(save_png(), "plot1.png")
   
-    # You'd then also provide a helper that skips tests where you can't
-    # be sure of producing exactly the same output
-    expect_snapshot_plot <- function(name, code) {
-      # Other packages might affect results
-      skip_if_not_installed("ggplot2", "2.0.0")
-      # Or maybe the output is different on some operation systems
-      skip_on_os("windows")
-      # You'll need to carefully think about and experiment with these skips
-      
-      name <- paste0(name, ".png")
-      
-      # Announce the file before touching `code`. This way, if `code`
-      # unexpectedly fails or skips, testthat will not auto-delete the
-      # corresponding snapshot file.
-      announce_snapshot_file(name = name)
-      
-      path <- save_png(code)
-      expect_snapshot_file(path, name)
-    }
-    
 })
 
 
